5595dc653ef6b3dcea2c6c15aad8d0bfbfc8cc21
Chayenne Jacques <Chayenne Jacques@Chayenne.telenet.be>
2016-10-24 12:28:47 +0200

    Aanmaken van de classes Game en Dealer Variabele bet(int) verwijderd vanuit de classe Hand en toegevoegd aan de classe User (want een Dealer heeft geen bet) Classes User en Headuser aangepast (Icon wordt niet meegegeven in de constructor)

--- BlackJack_Groep2/src/java/blackjack/model/User.java
+++ BlackJack_Groep2/src/java/blackjack/model/User.java
@@ -1,6 +1,9 @@
 
 package blackjack.model;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
  *
  * @author Chayenne Jacques
@@ -10,13 +13,15 @@ public class User {
     private String nickname;
     private int balance;
     private Icon icon;
+    private int bet;
     private Hand hand;
     private Gamestate state;
 
-    public User(String nickname, int balance, Icon icon) {
+    public User(String nickname, int balance) throws URISyntaxException {
         this.nickname = nickname;
         this.balance = balance;
-        this.icon = icon;
+        this.icon = new Icon("naam",new URI("path"));
+        this.hand = new Hand();
     }
     
     public void setIcon(Icon i){
@@ -25,6 +30,14 @@ public class User {
     
     public void setGamestate(Gamestate gs){
         this.state=gs;
+    }
+    
+    /**
+     * hoeveel credits een speler wil inzetten
+     * @param amount 
+     */
+    public void setBet(int amount){
+        this.bet=amount;
     }
     
     public Hand getHand(){
